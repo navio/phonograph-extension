052e9f1b53c85a11058574b6f8f98dad0e4d1113
Alberto Navarro <alberton@penguin.test>
2021-01-16 16:45:31 -0500

    Working images, not resized

--- src/ui/app/Library.tsx
+++ src/ui/app/Library.tsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
-import { AppContext} from "./index";
+import React, { useContext, useState } from "react";
+import { AppContext, IPodcast } from "./index";
+import imageFetcher from "../utils/imageSaver";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -8,31 +9,44 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
     images: {
-        width: `10vw`,
-        height: `10vw`
+      width: `10vw`,
+      height: `10vw`,
     },
     gridElement: {
-        backgroundColor: `red`,
-        width: `10vw`,
-        height: `10vw`,
-        display: `block`,
+      backgroundColor: `lightgray`,
+      width: `10vw`,
+      height: `10vw`,
+      display: `block`,
     },
     grid: {
-        display: `flex`,
-        flexFlow: `row wrap`,
-    }
+      display: `flex`,
+      flexFlow: `row wrap`,
+    },
   })
 );
 
+const PodcatImage = (props: { podcast: IPodcast }) => {
+  const { podcast } = props;
+  const classes = useStyles();
+  const [image, setImage] = useState<string>("");
+  React.useEffect(() => {
+    imageFetcher(podcast.image).then(setImage);
+  }, []);
+  return (
+    <a href={`#`} className={classes.gridElement}>
+      <img src={image} title={podcast.title} className={classes.images} />
+    </a>
+  );
+};
 
 export default () => {
-    const { collection } = useContext(AppContext);
-    const classes = useStyles()
-    return <div className={classes.grid}>
-    {collection.map((podcast) => (
-      <a href={`#`} className={classes.gridElement}>
-        <img src={podcast.image} title={podcast.title} className={classes.images} /> 
-      </a>
-    ))}
-  </div>
+  const { collection } = useContext(AppContext);
+  const classes = useStyles();
+  return (
+    <div className={classes.grid}>
+      {collection.map((podcast) => (
+        <PodcatImage podcast={podcast} />
+      ))}
+    </div>
+  );
 };
---
+++ src/ui/utils/imageSaver.ts
@@ -0,0 +1,46 @@
+import PodcastSuite from "podcastsuite";
+import PS from "podcastsuite";
+const { createDatabase } = PS;
+const images = createDatabase("images", "offline");
+
+interface MediaFile {
+  src: string;
+  size: number;
+  type: string;
+  url: string;
+}
+
+export default (url: string): Promise<string> => {
+  const internalFetch = () =>
+    fetch(url)
+      .then((res) => res.blob())
+      .then((blob) => {
+        return new Promise<string>((resolve) => {
+          const { size, type } = blob;
+          const reader = new FileReader();
+          reader.addEventListener("loadend", () => {
+            const src = reader.result.toString();
+
+
+            const mediaFile = {
+              src,
+              size,
+              type,
+              url,
+            };
+            // Store it ?
+            images.set(url, mediaFile);
+            resolve(mediaFile.src);
+          });
+          reader.readAsDataURL(blob);
+        });
+      });
+
+  return images.get(url).then((found: MediaFile, refresh = false) => {
+    //   console.log('what found', found);
+    if (!refresh && found) {
+      return found.src;
+    }
+    return internalFetch();
+  });
+};
