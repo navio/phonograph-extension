7e6b355a3388d9b17dac0f8525e965e16b2e289a
Alberto Navarro <alberton@penguin.test>
2021-01-18 12:42:27 -0500

    full call

--- src/ui/app/Podcast.tsx
+++ src/ui/app/Podcast.tsx
@@ -6,13 +6,23 @@ export default () => {
   const [podcast, setPodcast] = useState<IPodcast>();
   const { collection } = useContext(AppContext);
 
-  const { podcast: PodcastURL  } = useRouteMatch("/podcast/:podcast").params;
+  const { podcast: PodcastURL } = useRouteMatch("/podcast/:podcast").params;
   const url = atob(PodcastURL);
 
   useEffect(() => {
-    const [podcast] = collection.filter(podcast => podcast.url = url);
+    const [podcast] = collection.filter((podcast) => (podcast.url = url));
     setPodcast(podcast);
   }, [url, collection]);
 
-  return <div>Podcat: {podcast ? podcast.title: 'loading'} </div>;
+  return (
+    <div>
+      {podcast && (
+        <div>
+          {podcast.title}
+          <img src={podcast.image} />
+        </div>
+      )}
+      ;
+    </div>
+  );
 };
