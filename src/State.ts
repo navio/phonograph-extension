6c6538764f9c9a19e8528141ecb70bb5e1cbfbac
Alberto Navarro <alberton@penguin.test>
2021-01-26 22:07:59 -0500

    Updating State to support colors and Podcast.

--- src/State.ts
+++ src/State.ts
@@ -1,29 +1,39 @@
 import { podcasts } from "background/config";
+import PodcastSuite from "podcastsuite";
 import { IEpisode } from "podcastsuite/dist/Format";
+import { IPodcast } from "ui/app";
 import { IColor } from "ui/utils/color";
-import { PodcastImage } from "ui/utils/imageSaver";
+import FetchImage, { PodcastImage } from "ui/utils/imageSaver";
 import Podcast, { ISimplePodcast } from "./Podcast";
 
 
 const CURRENT_EPISODE = "current_episode";
 const CURRENT_PODCAST_INFO = "current_podcast_info";
 const CURRENT_PODCAST_IMAGE = "current_podcast_image";
-const CURRENT_PODCAST_COLORS = "current_podcast_colors";
 
 export interface IEpisodeState extends IEpisode {
   time: number;
 }
 
+
 export default class ApplicationState {
 
-  fetchSimplePodcast(url:String){
-
+  fetchSimplePodcast( podcast: IPodcast){
+    const {items, ...simplepodcast} = podcast;
+    FetchImage(simplepodcast.image).then(data => {
+      this.setPodcast(simplepodcast, data);
+    });
   }
   
-  setPodcast(podcastInfo: ISimplePodcast, podcastImage: PodcastImage, podcastColors: IColor){
+  setPodcast(podcastInfo: ISimplePodcast, podcastImage: PodcastImage){
     localStorage.setItem(CURRENT_PODCAST_INFO, JSON.stringify(podcastInfo));
     localStorage.setItem(CURRENT_PODCAST_IMAGE, JSON.stringify(podcastImage));
-    localStorage.setItem(CURRENT_PODCAST_COLORS, JSON.stringify(podcastColors));
+    return true;
+  }
+
+  clearPodcast(){
+    localStorage.removeItem(CURRENT_PODCAST_INFO)
+    localStorage.removeItem(CURRENT_PODCAST_IMAGE);
     return true;
   }
 
@@ -44,23 +54,16 @@ export default class ApplicationState {
     }
     return;
   }
-
-
-  getPodcastColors(): IColor | undefined{
-    const storedCurrent: string = localStorage.getItem(CURRENT_PODCAST_IMAGE);
-    if (storedCurrent) {
-      const currentObj: IColor = JSON.parse(storedCurrent);
-      return currentObj;
-    }
-    return;
-  }
     
-  setEpisode(episode: IEpisodeState) {
+  setEpisode(episode: IEpisodeState, podcast?: IPodcast) {
     localStorage.setItem(CURRENT_EPISODE, JSON.stringify(episode));
+    if(podcast){
+      this.fetchSimplePodcast(podcast);
+    }
     return true;
   }
 
-  getEpisode() {
+  getEpisode(podcast?: boolean, images?: boolean) {
     const storedCurrent: string = localStorage.getItem(CURRENT_EPISODE);
     if (storedCurrent) {
       const currentObj: IEpisodeState = JSON.parse(storedCurrent);
