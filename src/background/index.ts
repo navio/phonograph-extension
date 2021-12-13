9c83e5b4ca7bb6ce6bca5e9987f35384295fcccc
navio <albertonavarro@compass.com>
2021-12-12 20:06:31 -0500

    Adding External Permissions

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,7 +3,6 @@ import initPlayer from "player/reducer";
 import playlist from "playlist/reducer";
 import Engine from "lib/Podcast";
 import ApplicationState from "lib/State";
-import { podcasts } from "./config";
 import AudioElement from "lib/Audio";
 import browser from "lib/Browser";
 import Memory from "lib/Memory";
@@ -15,7 +14,7 @@ browser.setClickToPopup();
 
 // Global Objects
 const memory = new Memory();
-const engine = new Engine({ podcasts: podcasts });
+const engine = new Engine({ podcasts: [] });
 const state = new ApplicationState();
 const player = new AudioElement();
 const queue = new Queue();
